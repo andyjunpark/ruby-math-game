ca039558c7df096783ded8b76a0ecd5dd32705d2
Andy Junhyuk Park <pos03142ap@gmail.com>
2022-02-02 00:07:37 +0000

    implement game.rb

--- player.rb
+++ player.rb
@@ -1,7 +1,14 @@
 class Player 
-  attr_accessor :name :lives
+  attr_accessor :name, :lives
   def initialize(name, lives) 
     @name = name
     @lives = lives
   end
 end
+
+# player1 = Player.new('Player1', '3')
+# puts player1.name
+# puts player1.lives
+# player2 = Player.new('Player2', '3')
+# puts player2.name
+# puts player2.lives
